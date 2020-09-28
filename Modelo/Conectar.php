3dd6b69a1d099ffc596c35f1726eeb30a94a6a87
LopezJuanAndres <andres.juan.lopez@gmail.com>
2020-09-28 12:19:37 -0300

    actualizacion contraseña base de datos

--- Modelo/Conectar.php
+++ Modelo/Conectar.php
@@ -2,7 +2,7 @@
 class Conectar{
     public static function conexion(){
         try{
-            $conexion=new PDO ('mysql:host=localhost; dbname=login','root','andresroot');
+            $conexion=new PDO ('mysql:host=localhost; dbname=login','root','admin123');
             $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $conexion->exec("SET CHARACTER SET UTF8");
         } catch (Exception  $e) {
